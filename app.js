50968689e544a1eb13316d2ec5d62c468e1a0294
Briuwu <93917434+Briuwu7474@users.noreply.github.com>
2022-01-02 17:18:39 +0800

    initial

---
+++ app.js
@@ -0,0 +1,8 @@
+const shareBtn = document.querySelectorAll('#shareBtn')
+const share = document.querySelector('.share')
+
+shareBtn.forEach(btn => {
+  btn.addEventListener('click', () => {
+    share.classList.toggle('active')
+  })
+})
